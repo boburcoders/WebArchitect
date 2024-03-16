f3cda0c292dccae0bf0b73a24ce278943b4fbb19
boburcoders <boburtoshniyozov4@gmail.com>
2024-03-16 13:28:42 +0500

    add img

--- main.py
+++
@@ -1,5 +0,0 @@
-import requests
-city = input("Enter City: ")
-url = f"https://webarchitect.onrender.com/weather/{city}/"
-response = requests.get(url)
-print(response.text)
--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,24 @@
+let city = "Manchester"; // Since you're fetching weather data for Tashkent
 
-let city = "Tashkent";
-let api_key = 'b19ef47e383821acf92694c11bf2f394';
-let lat = 53.4795;
-let lon = -2.2451;
+  function getWeather() {
+    fetch(`http://127.0.0.1:8000/weather/${city}/`)
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data);
 
-function getWeather() {
-  //get lat and lon
-  fetch(`https://webarchitect.onrender.com/weather/${city}/`)
-    // Convert response string to json object
-    .then(response => response.json())
-    .then(response => {
+        let temperature = data['temperature'];
+        let clouds = data['description'];
+        let pressure = data['pressure'];
+        console.log(temperature)
 
-
-      let temperature = response['temp'] - 273.15;
-      let format_temp = temperature.toFixed(1);
-      let clouds = response['description'];
-      let pressure = response['pressure'];
-
-      console.log(response)
-
-      // Copy one element of response to our HTML paragraph
-      document.getElementById("get_temp").innerHTML = format_temp + "°C";
-      document.getElementById("icon").src = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
-      document.getElementById("myclouds").innerHTML = clouds.toUpperCase();
-      document.getElementById("pressure").innerHTML = pressure+"hPa";
-      console.log(response)
-
-    })
-    .catch(err => {
-      // Display errors in console
-      console.log(err);
-    });
-}
-
-
+        // Update HTML elements with weather data
+        document.getElementById("get_temp").innerHTML = temperature.toFixed(1) + "°C";
+        document.getElementById("myclouds").innerHTML = clouds.toUpperCase();
+        document.getElementById("pressure").innerHTML = pressure+"hPa";
+      })
+      .catch(err => {
+        // Display errors in console
+        console.log(err);
+      });
+  }
 
