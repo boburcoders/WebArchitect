b04953d5df9903426cfad96a6d60a4c46136b88f
boburcoders <boburtoshniyozov4@gmail.com>
2024-03-16 14:55:59 +0500

    change js file and index

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,21 @@
 function getWeather() {
-    let city = document.getElementById("city_input").value; // Get city from input field
+    let city = document.getElementById("city_input").value;
 
     fetch(`https://webarchitect.onrender.com/weather/${city}/`)
       .then(response => response.json())
       .then(data => {
-        // Update city name in header
-        document.getElementById("city_name").innerHTML = city.toUpperCase();
+
 
         let temperature = data['temperature'];
         let clouds = data['description'];
         let pressure = data['pressure'];
         console.log(temperature)
 
-        // Update HTML elements with weather data
         document.getElementById("get_temp").innerHTML = temperature.toFixed(1) + "°C";
         document.getElementById("myclouds").innerHTML = clouds.toUpperCase();
         document.getElementById("pressure").innerHTML = pressure+"hPa";
       })
       .catch(err => {
-        // Display errors in console
         console.log(err);
       });
   }
